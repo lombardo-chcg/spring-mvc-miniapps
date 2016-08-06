74039084162de6eea17cdc40ebba4b7ae2328a4f
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-06 13:45:13 -0500

    r-db

---
+++ relational-db/src/main/java/hello/Customer.java
@@ -0,0 +1,33 @@
+package hello;
+
+public class Customer {
+    private Long id;
+    private String firstName, lastName;
+
+    public Customer(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer #%d's name is %s %s", id, firstName, lastName);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
