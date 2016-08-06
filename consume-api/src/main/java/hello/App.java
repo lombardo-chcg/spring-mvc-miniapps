00d352ce58b19680e66c05f56ab2fdf63ada8d45
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-06 13:17:09 -0500

    Add consume api app

---
+++ consume-api/src/main/java/hello/App.java
@@ -0,0 +1,26 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
+    }
+}
