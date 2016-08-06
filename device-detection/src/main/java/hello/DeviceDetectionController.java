6d5e655a557257100bfc6447ccd9bb53c084f692
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-06 10:06:25 -0500

    add device detection app

---
+++ device-detection/src/main/java/hello/DeviceDetectionController.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.mobile.device.Device;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DeviceDetectionController {
+
+    @RequestMapping("/")
+    public @ResponseBody String detectDevice(Device device) {
+        String deviceType = "unknown";
+        if (device.isNormal()) {
+            deviceType = "desktop";
+        } else if (device.isMobile()) {
+            deviceType = "mobile";
+        } else if (device.isTablet()) {
+            deviceType = "tablet";
+        }
+        return "Hello " + deviceType + " Browser!";
+    }
+}
