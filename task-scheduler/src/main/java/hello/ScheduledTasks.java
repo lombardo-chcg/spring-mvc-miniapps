0537e2cf922011dc7da5e417b61ee06ece266684
lombardo-chcg <dpa.lombardo@gmail.com>
2016-08-06 09:25:04 -0500

    add task scheduler

---
+++ task-scheduler/src/main/java/hello/ScheduledTasks.java
@@ -0,0 +1,19 @@
+package hello;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+
+    public static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println(String.format("The time is now " + dateFormat.format(new Date())));
+    }
+
+}
